c53db475934f53fd4dba6f835e89c80972cf50c9
ongaeshi <ongaeshi0621@gmail.com>
2019-08-18 10:20:21 +0900

    test: test_empty_string

--- test.rb
+++ test.rb
@@ -1,2 +1,9 @@
-require_relative 'raylib-binding-code-generator-for-mruby'
+require_relative "raylib-binding-code-generator-for-mruby"
+require "test/unit"
 
+class ParserTest < Test::Unit::TestCase
+  def test_empty_string
+    parser = Parser.new("")
+    assert_equal "", parser.header_content
+  end
+end
