751c986b2fd4c35ca9c95b2d5d1af30cc94af5e5
ongaeshi <ongaeshi0621@gmail.com>
2019-08-18 20:11:30 +0900

    feat: Read from file

--- raylib-binding-code-generator-for-mruby.rb
+++ raylib-binding-code-generator-for-mruby.rb
@@ -2,18 +2,6 @@ require 'yaml'
 require_relative 'parser'
 
 if $0 == __FILE__
-  parser = Parser.new(<<-EOS
-  - name: struct Color
-    field:
-    - unsigned char r;
-    - unsigned char g;
-    - unsigned char b;
-    - unsigned char a;
-  - name: struct Vector2
-  - name: struct Vector3
-  - name: struct Camera
-  EOS
-  )
-
+  parser = Parser.new(File.read(ARGV[0]))
   puts parser.impl_content
 end
