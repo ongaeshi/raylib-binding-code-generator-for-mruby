dc639f819b9ad2305cdb69f46ac41bee9dff9eeb
ongaeshi <ongaeshi0621@gmail.com>
2019-08-18 17:15:05 +0900

    feat: Refer name parameter

--- type.rb
+++ type.rb
@@ -1,20 +1,24 @@
 class Type
+  attr_reader :name, :lower_name
+
   def initialize(name)
+    @name = name
+    @lower_name = name.downcase
   end
 
   def impl_header
     <<-EOS
-const static struct mrb_data_type mrb_raylib_color_type = { "Color", mrb_free };
+const static struct mrb_data_type mrb_raylib_#{lower_name}_type = { "#{name}", mrb_free };
 
 static mrb_value
-mrb_raylib_color_initialize(mrb_state *mrb, mrb_value self)
+mrb_raylib_#{lower_name}_initialize(mrb_state *mrb, mrb_value self)
 {
-    Color *obj;
+    #{name} *obj;
 
-    obj = (Color*)mrb_malloc(mrb, sizeof(Color));
-    memset(obj, 0, sizeof(Color));
+    obj = (#{name}*)mrb_malloc(mrb, sizeof(#{name}));
+    memset(obj, 0, sizeof(#{name}));
 
-    DATA_TYPE(self) = &mrb_raylib_color_type;
+    DATA_TYPE(self) = &mrb_raylib_#{lower_name}_type;
     DATA_PTR(self) = obj;
     return self;
 }
@@ -24,9 +28,9 @@ mrb_raylib_color_initialize(mrb_state *mrb, mrb_value self)
   def impl_content
     <<-EOS
     {
-        struct RClass *cls = mrb_define_class_under(mrb, mod_raylib, "Color", mrb->object_class);
+        struct RClass *cls = mrb_define_class_under(mrb, mod_raylib, "#{name}", mrb->object_class);
         MRB_SET_INSTANCE_TT(cls, MRB_TT_DATA);
-        mrb_define_method(mrb, cls, "initialize", mrb_raylib_color_initialize, MRB_ARGS_NONE());
+        mrb_define_method(mrb, cls, "initialize", mrb_raylib_#{lower_name}_initialize, MRB_ARGS_NONE());
     }
     EOS
   end
