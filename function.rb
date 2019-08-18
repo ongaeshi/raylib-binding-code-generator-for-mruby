a6f5d6b0ea4d9a6d3fba82edc2a08d45b79b221d
ongaeshi <ongaeshi0621@gmail.com>
2019-08-19 00:40:36 +0900

    feat: Impliment c_name, ret_type, arguments

--- function.rb
+++ function.rb
@@ -1,13 +1,26 @@
 class Function
-  attr_reader :c_name, :ruby_name, :arguments
+  attr_reader :c_name, :ruby_name
+  attr_reader :ret_type
+  attr_reader :arguments
 
   def initialize(src)
-    @c_name = "CloseWindow"
+    ret_type, c_name, arguments = src.scan(/([\w ]+?)(\w+)\((.*)\)/)[0]
+
+    @c_name = c_name
     @ruby_name = "close_window"
-    @arguments = []
+    @ret_type = parse_ret_type(ret_type)
+    @arguments = parse_arguments(arguments)
   end
 
-  def ret_type
-    "void"
+  def parse_ret_type(ret_type)
+    ret_type.strip
+  end
+
+  def parse_arguments(arguments)
+    arguments = arguments.strip
+    
+    return [] if (arguments == "void")
+    
+    raise
   end
 end
