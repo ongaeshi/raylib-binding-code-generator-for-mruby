3fc000524cc15a634e14d4ccec722c84f177faa3
ongaeshi <ongaeshi0621@gmail.com>
2019-08-18 16:41:37 +0900

    refactor: Divide type.rb

---
+++ type.rb
@@ -0,0 +1,35 @@
+class Type
+  def initialize(name)
+  end
+
+  def impl_header
+    <<-EOS
+const static struct mrb_data_type mrb_raylib_color_type = { "Color", mrb_free };
+
+static mrb_value
+mrb_raylib_color_initialize(mrb_state *mrb, mrb_value self)
+{
+    Color *obj;
+
+    obj = (Color*)mrb_malloc(mrb, sizeof(Color));
+    memset(obj, 0, sizeof(Color));
+
+    DATA_TYPE(self) = &mrb_raylib_color_type;
+    DATA_PTR(self) = obj;
+    return self;
+}
+    EOS
+  end
+
+  def impl_content
+    <<-EOS
+
+    {
+        struct RClass *cls = mrb_define_class_under(mrb, mod_raylib, "Color", mrb->object_class);
+        MRB_SET_INSTANCE_TT(cls, MRB_TT_DATA);
+        mrb_define_method(mrb, cls, "initialize", mrb_raylib_color_initialize, MRB_ARGS_NONE());
+    }
+    EOS
+  end
+end
+
