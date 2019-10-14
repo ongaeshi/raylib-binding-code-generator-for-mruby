c4144bbc105d5c1f30851dd575f33158198af371
ongaeshi <ongaeshi0621@gmail.com>
2019-10-14 15:43:57 +0900

    style: Split

--- util.rb
+++ util.rb
@@ -1,3 +1,15 @@
 def raylib_objects
-  %w(Vector2 Vector3 Vector4 Matrix Color Rectangle Image Texture2D Camera2D Camera3D Font)
+  %w(
+    Vector2
+    Vector3
+    Vector4
+    Matrix
+    Color
+    Rectangle
+    Image
+    Texture2D
+    Camera2D
+    Camera3D
+    Font
+    )
 end
